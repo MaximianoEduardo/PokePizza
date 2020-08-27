2bc65fd8d86dd0c0fb22ea87c5ccc5cdbc1f56e3
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-27 20:13:25 -0300

    Inital Structure 🖌

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Main from './page/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Poke Pizza</h1>
+      <Main />
     </div>
   );
 }
