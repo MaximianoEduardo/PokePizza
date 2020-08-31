02cdb4a8fce23c6b2620e629e873aa70617e6080
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-30 22:46:05 -0300

    feat $Banner changed
    🔥 Styled Componts/ Banners Structure  🔥

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Pokemon from '../../assets/images/logo-banner.png'
+
+import { BannerDiv, ButtonOrder, Title, ImageBanner } from './styles'
+
+const Banner: React.FC = () => {
+  return (
+
+    <BannerDiv>
+
+      <ImageBanner src={Pokemon} alt='Pokemon'/>
+
+      <Title>
+        Pizzas e salgados - Você encontra aqui !!
+      </Title>
+
+      <ButtonOrder>
+        Quero pedir
+      </ButtonOrder>
+
+    </BannerDiv>
+
+  );
+};
+
+export default Banner;
